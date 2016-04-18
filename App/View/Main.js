0d9e44553c09478c4a3167a10095c3322d923c47
Jakes Lee <jakeslee66@gmail.com>
2016-04-18 22:12:30 +0800

    adjust the top-bar; add type area ui

--- App/View/Main.js
+++ App/View/Main.js
@@ -13,7 +13,7 @@ import React, {
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons';
 import Color from '../Common/Color';
-
+import HeaderBar from '../Component/HeaderBar';
 import Home from './TabView/Home.js';
 
 export default class Main extends Component {
@@ -21,6 +21,7 @@ export default class Main extends Component {
         super(props);
         this.state = {
             selectedTab: 'home',
+            showTopBar: true,
         }
     }
 
@@ -30,51 +31,76 @@ export default class Main extends Component {
 
     render() {
         return (
-            <TabBarIOS
-                tintColor={Color.tabBarColor.tabColor}
-                barTintColor={Color.tabBarColor.tabBgColor}
-                style={{backgroundColor:'#F6F6F6'}}
-                >
-                <Icon.TabBarItem
-                    name="home"
-                    title="首页"
-                    iconName="ios-home-outline"
-                    selectedIconName="ios-home"
-                    selected={this.state.selectedTab === 'home'}
-                    onPress={()=> {
-                        this.setState({
-                            selectedTab: 'home'
-                        });
-                    }} >
-                    {this._renderContent()}
-                </Icon.TabBarItem>
-                <Icon.TabBarItem
-                    name="order"
-                    title="订单"
-                    iconName="ios-list-outline"
-                    selectedIconName="ios-list"
-                    selected={this.state.selectedTab === 'order'}
-                    onPress={()=> {
-                        this.setState({
-                            selectedTab: 'order'
-                        });
-                    }}>
-                    {this._renderContent()}
-                </Icon.TabBarItem>
-                <Icon.TabBarItem
-                    name="me"
-                    title="我"
-                    iconName="ios-person-outline"
-                    selectedIconName="ios-person"
-                    selected={this.state.selectedTab === 'me'}
-                    onPress={()=> {
-                        this.setState({
-                            selectedTab: 'me'
-                        });
-                    }}>
-                    {this._renderContent()}
-                </Icon.TabBarItem>
-            </TabBarIOS>
+            <View style={{flex: 1, flexDirection: 'column'}}>  
+                <TabBarIOS
+                    tintColor={Color.tabBarColor.tabColor}
+                    barTintColor={Color.tabBarColor.tabBgColor}
+                    style={[styles.tabContainer, {top: this.state.showTopBar ? 56 : 0}]}
+                    >
+                    <Icon.TabBarItem
+                        name="home"
+                        title="首页"
+                        iconName="ios-home-outline"
+                        selectedIconName="ios-home"
+                        selected={this.state.selectedTab === 'home'}
+                        onPress={()=> {
+                            this.setState({
+                                selectedTab: 'home',
+                                showTopBar: true,
+                            });
+                        }} >
+                        {this._renderContent()}
+                    </Icon.TabBarItem>
+                    <Icon.TabBarItem
+                        name="order"
+                        title="订单"
+                        iconName="ios-list-outline"
+                        selectedIconName="ios-list"
+                        selected={this.state.selectedTab === 'order'}
+                        onPress={()=> {
+                            this.setState({
+                                selectedTab: 'order',
+                                showTopBar: true,
+                            });
+                        }}>
+                        {this._renderContent()}
+                    </Icon.TabBarItem>
+                    <Icon.TabBarItem
+                        name="me"
+                        title="我"
+                        iconName="ios-person-outline"
+                        selectedIconName="ios-person"
+                        selected={this.state.selectedTab === 'me'}
+                        onPress={()=> {
+                            this.setState({
+                                selectedTab: 'me',
+                                showTopBar: false,
+                            });
+                        }}>
+                        {this._renderContent()}
+                    </Icon.TabBarItem>
+                </TabBarIOS>
+                {(()=> {
+                    if (this.state.showTopBar) {
+                        return <HeaderBar style={styles.topBar} />;
+                    }
+                })()}
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    topBar: {
+        position:'absolute', 
+        top: 0, 
+        left: 0
+    },
+    tabContainer: {
+        backgroundColor:'#F6F6F6',
+        position:'absolute', 
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
+});
--- App/View/TabView/Home.js
+++ App/View/TabView/Home.js
@@ -3,23 +3,103 @@
 import React, {
     Component,
     StyleSheet,
+    ScrollView,
+    TouchableOpacity,
     View,
     Text,
-} from 'react-native'
+    Image,
+} from 'react-native';
 
-import HeaderBar from '../../Component/HeaderBar';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 export default class Home extends Component {
     
     render() {
         return (
-            <View>
-                <HeaderBar />
+            <View style={{flex: 1,}}>
+                <ScrollView style={styles.contentContainer} 
+                            contentContainerStyle={{paddingVertical: 8}} 
+                            automaticallyAdjustContentInsets={false} >
+                    <View style={styles.contentArea}>
+                        <View style={{flexDirection: 'row'}}>
+                            <TouchableOpacity style={styles.typeBtn}>
+                                <Image style={[styles.btnIcon, ]} 
+                                source={require('../../../App/Resources/Images/category-icons/under-const.png')} />
+                                <Text style={styles.btnText}>施工</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.typeBtn}>
+                                <Image style={[styles.btnIcon, ]} 
+                                    source={require('../../../App/Resources/Images/category-icons/edu.png')} />
+                                <Text style={styles.btnText}>家教</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.typeBtn}>
+                                <Image style={[styles.btnIcon, ]} 
+                                    source={require('../../../App/Resources/Images/category-icons/clock.png')} />
+                                <Text style={styles.btnText}>钟点工</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.typeBtn}>
+                                <Image style={[styles.btnIcon, ]} 
+                                    source={require('../../../App/Resources/Images/category-icons/car.png')} />
+                                <Text style={styles.btnText}>代驾</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{flexDirection: 'row', marginTop: 10}}>
+                            <TouchableOpacity style={styles.typeBtn}>
+                                <Image style={[styles.btnIcon, ]} 
+                                source={require('../../../App/Resources/Images/category-icons/shop-cart.png')} />
+                                <Text style={styles.btnText}>代购</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.typeBtn}>
+                                <Image style={[styles.btnIcon, ]} 
+                                    source={require('../../../App/Resources/Images/category-icons/MedCar.png')} />
+                                <Text style={styles.btnText}>送药上门</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.typeBtn}>
+                                <Image style={[styles.btnIcon, ]} 
+                                    source={require('../../../App/Resources/Images/category-icons/gift.png')} />
+                                <Text style={styles.btnText}>送礼</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.typeBtn}>
+                                <Image style={[styles.btnIcon, ]} 
+                                    source={require('../../../App/Resources/Images/category-icons/working.png')} />
+                                <Text style={styles.btnText}>代班</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+                
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    contentContainer: {
+        flexDirection: 'column',
+    },
+    contentArea: {
+        backgroundColor: '#FFF',
+        borderTopColor: '#d8d8d8',
+        borderBottomColor: '#d8d8d8',
+        borderTopWidth: 0.5,
+        borderBottomWidth: 0.5,
+        padding: 10,
+    },
     
+    typeBtn: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    btnIcon: {
+        width: 45,
+        height: 45,
+        padding: 2,
+        marginBottom: 4,
+    },
+    btnText: {
+        textAlign: 'center',
+        fontSize: 12,
+    }
 });
