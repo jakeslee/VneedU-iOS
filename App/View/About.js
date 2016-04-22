188b5055f1c80d1be884308a336b7854d49d95ee
Jakes Lee <jakeslee66@gmail.com>
2016-04-22 23:31:03 +0800

    add about page

---
+++ App/View/About.js
@@ -0,0 +1,37 @@
+import React, {
+    Component,
+    StyleSheet,
+    TextInput,
+    StatusBar,
+    ScrollView,
+    TouchableOpacity,
+    Image,
+    View,
+    Text,
+} from 'react-native';
+import Base from '../Common/Base';
+import { BorderStyles, ButtonStyles } from '../Common/Styles';
+import NavigatorBar from '../Component/NavigatorBar';
+
+export default class About extends Component {
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <NavigatorBar title='关于威尼优' {...this.props} />
+                <ScrollView bounces={false}>
+                    <Image source={require('../Resources/Images/bgImg/about.png')} 
+                        style={{alignItems: 'center', justifyContent: 'flex-end', width: Base.width}}>
+                        <Text style={{borderColor: '#979797', borderRadius: 5, borderWidth: 0.5, textAlign: 'center', color: '#979797', width: 65}}>
+                            v1.0.0
+                        </Text>
+                        <TouchableOpacity style={{marginTop: 150, marginBottom: 170}}>
+                            <Text style={{fontSize: 20, color: '#303030'}}>
+                                检查更新
+                            </Text>
+                        </TouchableOpacity>
+                    </Image>
+                </ScrollView>
+            </View>
+        )
+    }
+}
