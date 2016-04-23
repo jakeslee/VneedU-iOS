ee76c7042c396a230adcd30ddcdccbdf5e7ef530
Jakes Lee <jakeslee66@gmail.com>
2016-04-23 16:46:39 +0800

    给导航栏增加搜索的输入框

--- App/Component/NavigatorBar.js
+++ App/Component/NavigatorBar.js
@@ -3,6 +3,7 @@ import React, {
     StyleSheet,
     PropTypes,
     StatusBar,
+    TextInput,
     TouchableOpacity,
     View,
     Text,
@@ -13,10 +14,16 @@ import { NavigatorStyles } from '../Common/Styles';
 export default class NavigatorBar extends Component {
     static defaultProps = {
         title: '',
+        enableSearchBar: false,
+        searchBarText: '',
+        onSubmit: (v)=>{},
     }
     
     static propTypes = {
         title: PropTypes.string,
+        enableSearchBar: PropTypes.bool,
+        searchBarText: PropTypes.string,
+        onSubmit: PropTypes.func,
     }
     
     constructor(props) {
@@ -26,13 +33,40 @@ export default class NavigatorBar extends Component {
     render() {
         StatusBar.setBarStyle('light-content', true);
         return (
-            <View style={NavigatorStyles.navigatorBar}>
+            <View style={[NavigatorStyles.navigatorBar, {flexDirection: 'row'}]}>
                 <TouchableOpacity onPress={()=> this.props.navigator.pop()} style={{flexDirection: 'row'}}>
-                    <Icon name='chevron-left' color='#FFF' size={20} />
-                    <Text style={{color: '#FFF', fontSize: 18, marginLeft: 10, }}>{this.props.title}</Text>
+                    <Icon name='chevron-left' color='#FFF' size={20} style={{paddingRight: 10}} />
+                    {this.props.enableSearchBar ? null :
+                    <Text style={{color: '#FFF', fontSize: 18, }}>{this.props.title}</Text>}
                 </TouchableOpacity>
+                {this.props.enableSearchBar ?
+                <View style={styles.inputArea}>
+                    <Icon name='ios-search' color='#A6A3A3' size={20}/>
+                    <TextInput style={styles.input} placeholder={this.props.searchBarText} 
+                        onSubmitEditing={(event)=> this.props.onSubmit(event.nativeEvent.text)} /> 
+                </View>: null}
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    inputArea: {
+        borderRadius: 5,
+        backgroundColor: '#FFF',
+        marginLeft: 5,
+        marginRight: 15,
+        paddingHorizontal: 10,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    input: {
+        paddingHorizontal: 5,
+        paddingVertical: 5,
+        height: 30,
+        fontSize: 15,
+        flex: 1,
+    }
+});
 
