c600c020a3dff2dbc4e45fdcea036fc09bd97748
Jakes Lee <jakeslee66@gmail.com>
2016-04-26 15:08:09 +0800

    增加评论组件

---
+++ App/Component/CommentItem.js
@@ -0,0 +1,57 @@
+import React, {
+    Component,
+    PropTypes,
+    StyleSheet,
+    TouchableOpacity,
+    View,
+    Text,
+    Image,
+} from 'react-native';
+
+import moment from 'moment';
+import localeZh from 'moment/locale/zh-cn';
+import { BorderStyles, } from '../Common/Styles';
+
+moment.locale('zh-cn', localeZh);
+
+export default class CommentItem extends Component {
+    static defaultProps = {
+        relatedTime: false,
+    }
+    
+    static propTypes = {
+        rowData: PropTypes.shape({
+            id: PropTypes.string,
+            content: PropTypes.string,
+            area: PropTypes.string,
+            score: PropTypes.number,
+            avatar: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+            datetime: PropTypes.string,
+        }).isRequired,
+        relatedTime: PropTypes.bool,
+    }
+    
+    render() {
+        let datetime = this.props.relatedTime ? 
+                        moment(this.props.rowData.datetime, 'YYYY-MM-DD HH:mm:ss').fromNow()
+                        : this.props.rowData.datetime;
+        return (
+            <View style={[BorderStyles.top, {flexDirection: 'row', padding: 10, alignItems: 'center'}]}>
+                <Image style={{width: 31, height: 31}} source={this.props.rowData.avatar}/>
+                <View style={{marginLeft: 10, flex: 1}}>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <Text style={{flex: 1, color: '#323231'}}>
+                            来自{this.props.rowData.area}的用户
+                        </Text>
+                        <Text style={{color: '#6F6F6F', fontSize: 12}}>
+                            {datetime}
+                        </Text>
+                    </View>
+                    <Text style={{marginTop: 5}}>
+                        {this.props.rowData.content}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+}
